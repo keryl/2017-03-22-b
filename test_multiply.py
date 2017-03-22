478dbc6385fdf2eef12dfa07f6bb91df6bd9f468
keryl <kerylklonye@gmail.com>
2017-03-23 00:02:50 +0300

    add test_multiply

--- test_multiply.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import multiply
-
-class MultiplyTestCase(unittest.TestCase):
-    def _test_multiplies_numbers(self):
-        result = multiply.multiply(2, 3)
-        self.assertEqual(6, result)
