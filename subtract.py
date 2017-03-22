3d11a967862c25e9e4af49b878f468323d6731c7
keryl <kerylklonye@gmail.com>
2017-03-22 21:58:18 +0300

    minus function should accept numbers only

--- subtract.py
+++ subtract.py
@@ -1,2 +1,15 @@
+def isdigit(arg):
+    """ returns true if arg is an integer or float """
+    arg_is_int = type(arg) == int
+    arg_is_float = type(arg) == float
+    args_is_both_float_and_int = arg_is_int or arg_is_float
+    return args_is_both_float_and_int
+
 def minus(a, b):
-    return(a - b)
+    """ subtracts b from a if both are numbers """
+
+    if isdigit(a) and isdigit(b): # if a and b are both numbers
+        return(a - b) # subtract and return result
+    else: # else return error string
+        return("numbers only")
+
--- test_subtract.py
+++ test_subtract.py
@@ -1,13 +1,37 @@
 import unittest
 import subtract
 
-class SubtractTestcase(unittest.TestCase):
+class MinusTestcase(unittest.TestCase):
 
     def test_subtracts_numbers(self):
         result = subtract.minus(5, 3)
         self.assertEqual(2, result)
-        
 
+    def test_does_not_accept_strings(self):
+        result = subtract.minus("test", 3)
+        self.assertEqual("numbers only", result)
+
+    def test_does_not_accept_booleans(self):
+        result = subtract.minus(3, True)
+        self.assertEqual("numbers only", result)
+
+class IsDigitTestCase(unittest.TestCase):
+
+    def test_it_returns_true_if_int(self):
+        result = subtract.isdigit(2)
+        self.assertTrue(result)
+
+    def test_it_returns_true_if_float(self):
+        result = subtract.isdigit(1.2)
+        self.assertTrue(result)
+
+    def test_it_returns_false_if_string(self):
+        result = subtract.isdigit("five")
+        self.assertFalse(result)
+
+    def test_it_returns_false_if_boolean(self):
+        result = subtract.isdigit(True)
+        self.assertFalse(result)
 
 if __name__ =="__main__":
     unittest.main()
